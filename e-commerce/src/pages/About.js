34ce2cc9e4d021becbdae5a11fcbfc0b85982647
Neskines <123345003+neski321@users.noreply.github.com>
2024-06-18 18:41:36 +0000

    added logout function and contact and about pages

---
+++ e-commerce/src/pages/About.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function About(){
+    return(
+        <p>Coming soon</p>
+    );
+}
+export default About;
--- e-commerce/src/pages/Home.js
+++ e-commerce/src/pages/Home.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import Categories from '../components/Categories';
@@ -11,9 +10,9 @@ function Home() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:8000/api/products/')
+    axios.get('https://dummyjson.com/products')
       .then(response => {
-        setProducts(response.data);
+        setProducts(response.data.products);
       })
       .catch(error => {
         console.error('There was an error fetching the products!', error);
