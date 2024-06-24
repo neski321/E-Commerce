51f6eed1bcc946477212c59b96997cf15466b2a0
neskines <nmotieno@myseneca.ca>
2024-06-24 14:40:19 -0400

    changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from dotenv import load_dotenv
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,14 +85,7 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DATABASE_NAME'),
-        'USER': os.getenv('DATABASE_USER'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': os.getenv('DATABASE_HOST'),
-        'PORT': os.getenv('DATABASE_PORT'),
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
--- e-commerce/src/services/productService.js
+++ e-commerce/src/services/productService.js
@@ -1,11 +1,10 @@
 // src/services/productService.js
 import axios from 'axios';
 
-const API_BASE_URL = 'https://e-commerce-6zf9.onrender.com/api';
 
 export const fetchProducts = async () => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/products/`);
+    const response = await axios.get('http://localhost:8000/api/products/');
     return response.data;
   } catch (error) {
     console.error('Error fetching products:', error);
