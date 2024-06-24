fb755d0f829d9b0ff0708c77ed2792bec57d33f1
neskines <nmotieno@myseneca.ca>
2024-06-24 00:35:30 -0400

    Update backend

--- e-commerce/src/services/productService.js
+++ e-commerce/src/services/productService.js
@@ -1,9 +1,11 @@
 // src/services/productService.js
 import axios from 'axios';
 
+const API_BASE_URL = 'https://e-commerce-6zf9.onrender.com/api';
+
 export const fetchProducts = async () => {
   try {
-    const response = await axios.get('http://localhost:8000/api/products/');
+    const response = await axios.get(`${API_BASE_URL}/products/`);
     return response.data;
   } catch (error) {
     console.error('Error fetching products:', error);
