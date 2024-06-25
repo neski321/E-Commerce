8a68bc6a16077fbf200ce56f581d946e27630a76
Neskines <123345003+neski321@users.noreply.github.com>
2024-06-25 15:44:47 +0000

    Linked backend to react

--- e-commerce/src/services/productService.js
+++ e-commerce/src/services/productService.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 export const fetchProducts = async () => {
   try {
-    const response = await axios.get('http://localhost:8000/api/products/');
+    const response = await axios.get('https://e-commerce-6zf9.onrender.com/api/products/');
     return response.data;
   } catch (error) {
     console.error('Error fetching products:', error);
