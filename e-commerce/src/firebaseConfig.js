6aac7b27af1096f2b447dbd7e99509e1f37841bf
neskines <nmotieno@myseneca.ca>
2024-07-02 08:12:17 -0400

    Changes made

--- e-commerce/src/firebaseConfig.js
+++ e-commerce/src/firebaseConfig.js
@@ -3,14 +3,14 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDGdUSxHwKUGL4NWaQq_z6Ex4KSgxlWHig",
-    authDomain: "e-commerce-3d587.firebaseapp.com",
-    projectId: "e-commerce-3d587",
-    storageBucket: "e-commerce-3d587.appspot.com",
-    messagingSenderId: "576725083107",
-    appId: "1:576725083107:web:9641344343363d0bb4f6d6"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
   };
-  
+
   // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
