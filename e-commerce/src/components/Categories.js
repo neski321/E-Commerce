22acd1bc188091d15457b1debddae1d572d139ed
neskines <nmotieno@myseneca.ca>
2024-07-02 11:04:25 -0400

    Changes

--- e-commerce/src/components/Categories.js
+++ e-commerce/src/components/Categories.js
@@ -5,6 +5,11 @@ import { Link } from 'react-router-dom';
 
 const Categories = () => {
   const [categories, setCategories] = useState([]);
+  const [showCategories, setShowCategories] = useState(false);
+
+  const toggleCategories = () => {
+    setShowCategories(!showCategories);
+  };
 
   useEffect(() => {
     const loadCategories = async () => {
@@ -22,16 +27,20 @@ const Categories = () => {
 
   return (
     <div className="px-6 py-8">
-      <h2 className="text-2xl font-bold mb-4">Shop by Category</h2>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        {categories.map((category, index) => (
-          <Link to={`/category/${category.toLowerCase()}`} key={index} className="block group">
-            <div className="border rounded p-4 hover:bg-gray-100 transition duration-300 ease-in-out">
-              <h3 className="text-xl font-semibold text-gray-800 group-hover:text-gray-600">{category}</h3>
-            </div>
-          </Link>
-        ))}
-      </div>
+      <button onClick={toggleCategories} className="text-2xl font-bold mb-4">
+            Shop by Category
+          </button>
+      {showCategories && (
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          {categories.map((category, index) => (
+            <Link to={`/category/${category.toLowerCase()}`} key={index} className="block group">
+              <div className="border rounded p-4 hover:bg-gray-100 transition duration-300 ease-in-out">
+                <h3 className="text-xl font-semibold text-gray-800 group-hover:text-gray-600">{category}</h3>
+              </div>
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
