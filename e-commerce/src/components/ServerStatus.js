350fb744440d3d5f15dd30f2fab5f6699e3b1d87
neskines <nmotieno@myseneca.ca>
2024-07-02 02:08:33 -0400

    Updated serverstatus

--- e-commerce/src/components/ServerStatus.js
+++ e-commerce/src/components/ServerStatus.js
@@ -1,28 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const ServerStatus = () => {
-    const [status, setStatus] = useState('');
+  const [status, setStatus] = useState('Checking...');
 
-    useEffect(() => {
-        axios.get('/server-status/')
-            .then(response => {
-                setStatus(response.data.status);
-            })
-            .catch(error => {
-                setStatus('Error fetching server status');
-                console.error('There was an error fetching the server status!', error);
-            });
-    }, []);
+  useEffect(() => {
+    axios.get('https://e-commerce-6zf9.onrender.com/server-status/')
+      .then(response => {
+        setStatus(response.data.status);
+      })
+      .catch(error => {
+        setStatus('Error: Unable to fetch server status');
+      });
+  }, []);
 
-    return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-100">
-            <div className="bg-white p-6 rounded-lg shadow-lg">
-                <h1 className="text-2xl font-bold mb-4">Server Status</h1>
-                <p className="text-gray-700">{status}</p>
-            </div>
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="bg-white p-6 rounded-lg shadow-lg">
+            <h1 className="text-2xl font-bold mb-4">Server Status</h1>
+            <p className="text-gray-700">{status}</p>
         </div>
-    );
-}
+    </div>
+  );
+};
 
 export default ServerStatus;
