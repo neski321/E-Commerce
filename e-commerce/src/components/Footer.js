b03db206dabbbaeedcd102234ca4aa3787c47d8d
neskines <nmotieno@myseneca.ca>
2024-07-03 16:45:10 -0400

    visual

--- e-commerce/src/components/Footer.js
+++ e-commerce/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 py-6 mt-12">
       <div className="container mx-auto text-center text-white">
-        <p>&copy; 2024 E-commerce Store. All rights reserved.</p>
+        <p>&copy; 2024 E-commerce Store by Neski. All rights reserved.</p>
         <div className="mt-4">
           <Link to="/about" className="text-yellow-400 hover:underline mx-2">About</Link>
           <Link to="/contact" className="text-yellow-400 hover:underline mx-2">Contact</Link>
