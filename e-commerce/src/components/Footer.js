6b5dee8c04060d36b1217710080d24b6b5f1a658
neskines <nmotieno@myseneca.ca>
2024-07-03 15:58:51 -0400

    minor fixes

--- e-commerce/src/components/Footer.js
+++ e-commerce/src/components/Footer.js
@@ -1,6 +1,6 @@
 // src/components/Footer.js
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
--- e-commerce/src/pages/SearchPage.js
+++ e-commerce/src/pages/SearchPage.js
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+
+const API_URL = process.env.REACT_APP_API_URL;
 
 const SearchPage = () => {
   const [query, setQuery] = useState('');
