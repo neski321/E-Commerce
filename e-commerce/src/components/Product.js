ec8ccb5a6a33c60f65156dadea64b886062b5ce4
Neskines <123345003+neski321@users.noreply.github.com>
2024-07-04 18:14:07 +0000

    adding favorites 1

--- e-commerce/src/components/Product.js
+++ e-commerce/src/components/Product.js
@@ -2,11 +2,14 @@
 import React, { useEffect, useState } from 'react';
 import { fetchProducts } from '../services/productService';
 import { Link } from 'react-router-dom';
+import { auth , db } from '../firebaseConfig';
+import { collection, addDoc, getDocs, query, where } from "firebase/firestore";
 
 const Product = () => {
   const [products, setProducts] = useState([]);
   const [showFeaturedProducts, setShowFeaturedProducts] = useState(false);
   const [loaded, setLoaded] = useState(false);
+  const [favorites, setFavorites] = useState([]);
 
   useEffect(() => {
     const loadProducts = async () => {
@@ -17,14 +20,50 @@ const Product = () => {
         console.error('Error loading products:', error);
       }
     };
+
+    const loadFavorites = async () => {
+      const user = user.currentUser;
+      if (user) {
+        const favoritesRef = collection(db, 'favorites', user.uid, 'products');
+        const querySnapshot = await getDocs(favoritesRef);
+        const favoriteProducts = querySnapshot.docs.map(doc => doc.data().productId);
+        setFavorites(favoriteProducts);
+      }
+    }
+
     if (!loaded) {
       loadProducts();
+      loadFavorites();
       setLoaded(true);
     }
     loadProducts();
   }, [loaded]);
 
   const toggleFeaturedProducts = () => setShowFeaturedProducts(!showFeaturedProducts);
+
+  const addToFavorites = async (product) => {
+    const user = user.currentUser;
+    if (user) {
+      try {
+        const favoritesRef = collection(db, 'favorites', user.uid, 'products');
+        await addDoc(favoritesRef, {
+          productId: product.id,
+          name: product.title,
+          description: product.description,
+          price: product.price,
+          image: [product.thumbnail]
+        });
+        setFavorites([...favorites, product.Id]);
+        alert('Added to favorites');
+      } catch (error) {
+        console.error('Error adding to favorites:', error);
+      }
+    }else {
+      alert('You need to be logged in');
+    }   
+  };
+
+  const isFavorite = (productId) => favorites.includes(productId);
 
 
   return (
@@ -44,6 +83,14 @@ const Product = () => {
               <button className="bg-gray-200 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded"><Link to={`/products/${product.id}`} className="text-blue-500 hover:underline">
                 View Details
               </Link></button>
+              <br />
+              <button
+                onClick={() => addToFavorites(product)}
+                className={`mt-2 text-white px-4 py-2 rounded ${isFavorite(product.id) ? 'bg-gray-500' : 'bg-red-500'}`}
+                disabled={isFavorite(product.id)}
+                >
+                  {isFavorite(product.id) ? 'In favorites' : 'Add to favorites'}
+                </button>
             </div>
           ))}
         </div>
