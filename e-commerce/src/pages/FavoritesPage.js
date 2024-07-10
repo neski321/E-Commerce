f2eba97e07c8431c1417e36c17020260e89590de
Neskines <123345003+neski321@users.noreply.github.com>
2024-07-10 18:46:07 +0000

    fixed error

--- e-commerce/src/pages/FavoritesPage.js
+++ e-commerce/src/pages/FavoritesPage.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {auth, db} from '../firebaseConfig';
 import { collection, getDocs } from "firebase/firestore";
-import Product from '../components/Product';
+import { Link } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
