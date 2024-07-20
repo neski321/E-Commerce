fcb055e483c6d2e676b7ade8917018d57557b1ac
neskines <nmotieno@myseneca.ca>
2024-07-20 07:28:32 -0400

    Added more fields to products

--- backend/products/models.py
+++ backend/products/models.py
@@ -4,14 +4,35 @@ from django.db import models
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.CharField(max_length=255)
+    discount_percentage = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    rating = models.FloatField(default=0.0)
+    stock = models.IntegerField(default=0)
+    brand = models.CharField(max_length=100, default='N/A')
+    sku = models.CharField(max_length=50, default='N/A')
+    weight = models.FloatField(default=0.0)
+    warranty_information = models.CharField(max_length=255, default='To be determined')
+    shipping_information = models.CharField(max_length=255, default='To be determined')
+    availability_status = models.CharField(max_length=50, default='In progress')
+    return_policy = models.CharField(max_length=255, default='To be determined')
+    minimum_order_quantity = models.IntegerField(default=1)
     thumbnail = models.URLField(default='XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
-    description = models.TextField(default='To be determined')
-    availabilityStatus = models.CharField(max_length=255, default='In progress')
-    images = models.JSONField(default=dict)  # Assuming images is a list of URLs
-    discount_percentage = models.FloatField(default=0.0)
-    warranty_information = models.TextField(default='To be determined')
     
     def __str__(self):
         return self.title
+    
+class Review(models.Model):
+    product = models.ForeignKey(Product, related_name='reviews', on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    date = models.DateTimeField()
+    reviewer_name = models.CharField(max_length=100)
+    reviewer_email = models.EmailField()
+
+class Dimension(models.Model):
+    product = models.OneToOneField(Product, on_delete=models.CASCADE, related_name='dimensions')
+    width = models.FloatField()
+    height = models.FloatField()
+    depth = models.FloatField()
--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Review, Dimension
+
+class DimensionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Dimension
+        fields = ('width', 'height', 'depth')
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
 
 class ProductSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True, read_only=True)
+    dimensions = DimensionSerializer()
+
     class Meta:
         model = Product
         fields = '__all__'
