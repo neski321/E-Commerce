e90b7df450d062c8eb31bd03b1c4134b93895589
neskines <nmotieno@myseneca.ca>
2024-09-22 00:15:14 -0400

    added update to views

--- backend/products/views.py
+++ backend/products/views.py
@@ -74,3 +74,19 @@ def add_product(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     else:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['PUT'])
+def update_product(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = ProductSerializer(product, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        # Debug: Print the errors in the console
+        print(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
